c6e6bebe776ff5c143405d3847b45c7eaf0c5a52
atul1171 <atul@intellect.co>
2023-04-10 12:25:32 +0530

    Initial commit

--- src/config/databaseConfig.ts
+++ src/config/databaseConfig.ts
@@ -9,7 +9,6 @@ dotenv.config();
 const databaseConfig: TypeOrmModuleOptions = {
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: 5432,
   username: process.env.DATABASE_USERNAME,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
